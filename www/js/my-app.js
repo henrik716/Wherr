526c113dfad97c222cf5cef845c23704baed324d
Henrik <hgs716@gmail.com>
2017-11-02 18:56:41 +0100

    Changumappu

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,69 +1,69 @@
 document.addEventListener('deviceready', function() {
 
-    var success = function(response) {
-        // response.isGooglePlayServicesAvailable is a boolean value
-        response.isGooglePlayServicesAvailable;
-    }
+	var success = function(response) {
+		// response.isGooglePlayServicesAvailable is a boolean value
+		response.isGooglePlayServicesAvailable;
+	}
 
-    var failure = function(response) {
-        response.isGooglePlayServicesAvailable;
-    }
+	var failure = function(response) {
+		response.isGooglePlayServicesAvailable;
+	}
 
-    GooglePlayServicesCheck.check(success, failure);
+	GooglePlayServicesCheck.check(success, failure);
 
-    var div = document.getElementById("map_canvas");
-    var map = plugin.google.maps.Map.getMap(div, {
-        'mapType': plugin.google.maps.MapTypeId.ROADMAP,
-        'controls': {
-            'compass': true,
-            'myLocationButton': false,
-            'indoorPicker': true,
-            'zoom': true
-        },
-        'gestures': {
-            'scroll': true,
-            'tilt': true,
-            'rotate': true,
-            'zoom': true
-        },
-        'styles': [{
-            featureType: "all",
-            stylers: [{
-                saturation: -80
-            }]
-        }, {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [{
-                    hue: "#00ffee"
-                },
-                {
-                    saturation: 50
-                }
-            ]
-        }, {
-            featureType: "poi.business",
-            elementType: "labels",
-            stylers: [{
-                visibility: "off"
-            }]
-        }],
-        'camera': {
-            target: {
-                lat: 37.422375,
-                lng: -122.084207
-            },
-            zoom: 10
-        },
-        'preferences': {
-            'zoom': {
-                'minZoom': 10,
-                'maxZoom': 18
-            },
-            'building': false
-        }
-    });
-    map.one(plugin.google.maps.event.MAP_READY, function() {
-        console.log("--> map : ready.");
-    });
+	var div = document.getElementById("map_canvas");
+	var map = plugin.google.maps.Map.getMap(div, {
+		'mapType': plugin.google.maps.MapTypeId.HYBRID,
+		'controls': {
+			'compass': true,
+			'myLocationButton': false,
+			'indoorPicker': true,
+			'zoom': true
+		},
+		'gestures': {
+			'scroll': true,
+			'tilt': true,
+			'rotate': true,
+			'zoom': true
+		},
+		'styles': [{
+			featureType: "all",
+			stylers: [{
+				saturation: 0
+			}]
+		}, {
+			featureType: "road.arterial",
+			elementType: "geometry",
+			stylers: [{
+					hue: "#00ffee"
+				},
+				{
+					saturation: 50
+				}
+			]
+		}, {
+			featureType: "poi.business",
+			elementType: "labels",
+			stylers: [{
+				visibility: "off"
+			}]
+		}],
+		'camera': {
+			target: {
+				lat: 59.911491,
+				lng: 10.757933
+			},
+			zoom: 10
+		},
+		'preferences': {
+			'zoom': {
+				'minZoom': 3,
+				'maxZoom': 18
+			},
+			'building': false
+		}
+	});
+	map.one(plugin.google.maps.event.MAP_READY, function() {
+		console.log("--> map : ready.");
+	});
 });
