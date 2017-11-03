aaa1bb5a5104a8af65c0009abeb3aaf02c314af1
jaggum <jonasneraal@gmail.com>
2017-11-03 11:08:38 +0100

    pusher bare css fila med endret height i % og 0 margin

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-plugin-googleplayservices-check.GooglePlayServicesCheck",
+    "file": "plugins/cordova-plugin-googleplayservices-check/www/GooglePlayServicesCheck.js",
+    "pluginId": "cordova-plugin-googleplayservices-check",
+    "clobbers": [
+      "GooglePlayServicesCheck"
+    ]
+  },
+  {
     "id": "cordova-plugin-googlemaps.BaseClass",
     "file": "plugins/cordova-plugin-googlemaps/www/BaseClass.js",
     "pluginId": "cordova-plugin-googlemaps",
@@ -169,23 +177,15 @@ module.exports = [
     "clobbers": [
       "plugin.google.maps"
     ]
-  },
-  {
-    "id": "cordova-plugin-googleplayservices-check.GooglePlayServicesCheck",
-    "file": "plugins/cordova-plugin-googleplayservices-check/www/GooglePlayServicesCheck.js",
-    "pluginId": "cordova-plugin-googleplayservices-check",
-    "clobbers": [
-      "GooglePlayServicesCheck"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "com.googlemaps.ios": "2.4.0",
-  "cordova-plugin-googlemaps": "2.1.0-beta-20171027-0054",
+  "cordova-plugin-whitelist": "1.3.2",
   "cordova-plugin-googleplayservices-check": "1.0.0",
-  "cordova-plugin-whitelist": "1.3.2"
+  "com.googlemaps.ios": "2.4.0",
+  "cordova-plugin-googlemaps": "2.1.0-beta-20171027-0054"
 };
 // BOTTOM OF METADATA
 });
